f78d5ae7bc4ac5b40c60f83f55a7f35dee26e5e1
Mayuri Buckius <mbuckius@uci.edu>
2022-07-21 23:49:38 -0500

    Add testimonials

--- main.js
+++ main.js
@@ -2,11 +2,41 @@
 let header = document.querySelector('.header');
 let hamburgerMenu = document.querySelector('.hamburger-menu');
 
+//change header when user scrolls
 window.addEventListener('scroll', function () {
     let windowPosition = window.scrollY > 0;
     header.classList.toggle('active', windowPosition);
 })
 
+//change hamburger menu when user clicks on it
 hamburgerMenu.addEventListener('click', function () {
     header.classList.toggle('menu-open');
 })
+
+//show slideshow
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slide");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
